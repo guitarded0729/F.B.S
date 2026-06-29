9f94b569fef752fe0f1db72a9088fb8f7729415e
guitarded0729 <aryansingh0729@gmail.com>
2026-06-29 17:46:09 +0530

    Add files via upload

---
+++ food.java
@@ -0,0 +1,93 @@
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
+public class food
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+
+        //menu
+        String[] items={"Vada pav","Missal pav", "Samosa pav", "Tea  ", "Coffee", "Water bottle"};
+        int[] prices={30,40,25,15,20,10};
+        int[] quantity = new int[items.length];
+        double total=0;
+        double gtotal=0;
+
+        //take customer input
+        System.out.println("===Welcome to Shop===\n");
+
+        System.out.println("Enter customer name");
+        String name=sc.nextLine();
+
+        System.out.println("Enter phone number");
+        String number=sc.nextLine();
+
+        System.out.println("Enter address");
+        String address=sc.nextLine();
+
+        System.out.println("\n\t---Menu---");
+        for(int i=0;i<items.length;i++)
+        {
+            System.out.println((i+1) + ".)" + items[i] + "\t -₹."+prices[i]);
+        }
+
+
+        //taking order
+        while(true)
+        {
+            System.out.println("Enter item number to order(0 to end order)");
+            int choice=sc.nextInt();
+            if(choice == 0)
+            {
+                break;
+            }
+            if(choice < 1 || choice > items.length)
+            {
+                System.out.println("Invalid entry");
+                continue;
+            }
+            System.out.println("Enter quantity for "+ items[choice -1]+":");
+            int qty = sc.nextInt();
+            quantity[choice -1]+=qty;
+            System.out.println(items[choice-1]+" added to order");
+        }
+        //print bill
+        LocalDate date=LocalDate.now();        //print date and time
+        LocalTime time=LocalTime.now();
+
+        DateTimeFormatter formatD=DateTimeFormatter.ofPattern("dd/MM/yy");//format date 
+        String formattedD=date.format(formatD);
+
+        DateTimeFormatter formatT=DateTimeFormatter.ofPattern("HH:mm");//format time
+        String formattedT=time.format(formatT);
+        
+        System.out.println("\n================ FINAL BILL ================");
+        System.out.println("Date: "+formattedD);
+        System.out.println("Time: "+formattedT);
+        System.out.println("Customer name : "+name);
+        System.out.println("Phone number: "+ number);
+        System.out.println("Address : "+ address);  
+        System.out.println("--------------------------------------------------------");       
+        System.out.println("Item\t \tOuantity\t Price\t Amount \t GST");
+        System.out.println("--------------------------------------------------------"); 
+        
+        
+        for(int i=0;i< items.length;i++)
+        {
+            if(quantity[i] >0)
+            {
+                double amount=prices[i]*quantity[i];
+                total+=amount;
+                double gstamount=amount+((amount*5)/100);
+                gtotal+=gstamount;
+                System.out.println(items[i]+ "\t\t" + quantity[i]+ "\t" + prices[i] + "\t" + amount + "\t" +gstamount);    
+            }
+        }
+        System.out.println("-----------------------------------------------------------");
+        System.out.println("Grand Total: Rs."+gtotal);
+         //GST
+        
+    }
+}
